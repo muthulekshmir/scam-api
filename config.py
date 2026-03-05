098693cbbc8110e26fa334324efca2ba87968060
muthulekshmir <muthu123lekshmi@gmail.com>
2026-03-05 07:15:38 -0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
-    MODEL_PATH = os.environ.get('MODEL_PATH') or 'models/xgb_model.pkl'
-    TOKENIZER_PATH = os.environ.get('TOKENIZER_PATH') or 'models/distilbert_tokenizer'
-    DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-    TESTING = os.environ.get('TESTING', 'False') == 'True'
